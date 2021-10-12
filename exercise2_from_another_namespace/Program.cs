b6932821be6a0d0f8977f3114152eda631f5d7ab
AnnKemp <ann.kemp@scarlet.be>
2021-10-12 17:02:06 +0200

    Add project files.

---
+++ exercise2_from_another_namespace/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise2_from_another_namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Other useful array methods, such as Min, Max, and Sum, can be found in the System.Linq namespace
+            int[] myNumbers = { 5, 1, 8, 9 };
+            Console.WriteLine(myNumbers.Max());  // returns the largest value
+            Console.WriteLine(myNumbers.Min());  // returns the smallest value
+            Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
+        }
+    }
+}
